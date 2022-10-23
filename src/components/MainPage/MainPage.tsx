186d4bf5f714fcfffdce060841703f6f71a149e2
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-10-23 19:58:47 +0200

    styled-components installed and Container created

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,13 +1,20 @@
 import Modal from '../Modal/Modal';
 import Button from '@mui/material/Button';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 50px;
+`;
 
 const MainPage = () => {
 
     return(
-        <>
+        <Container>
             <Button variant='contained'>Open Modal</Button>
-            <Modal/>
-        </>
+            {/* <Modal/> */}
+        </Container>
     )
 };
 
