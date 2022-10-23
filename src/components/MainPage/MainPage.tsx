639669d4b5034b9f2345fabf9f81b8d13403e6c2
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-10-23 18:41:26 +0200

    Roboto installed and Button created in MainPage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,11 @@
 import Modal from '../Modal/Modal';
+import Button from '@mui/material/Button';
 
 const MainPage = () => {
 
     return(
         <>
+            <Button variant='contained'>Open Modal</Button>
             <Modal/>
         </>
     )
