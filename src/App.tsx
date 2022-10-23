8ddeeef2261e6bba14251aad8ea6a9ef43db622f
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-10-23 18:10:45 +0200

    React router installed and puppet components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 
+//REACT ROUTER
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+//COMPONENTS IMPORTS
+import MainPage from './components/MainPage';
+import NextPage from './components/NextPage';
+
 function App() {
   return (
-    <div className="App">
-      TEST?
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage/>}/>
+          <Route path='modal' element={<MainPage/>}/>
+          <Route path='/nextPage' element={<NextPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/NextPage.tsx
@@ -0,0 +1,11 @@
+
+const NextPage = () => {
+
+    return(
+        <>
+            <h1>THIS IS NEXT PAGE</h1>
+        </>
+    )
+};
+
+export default NextPage;
